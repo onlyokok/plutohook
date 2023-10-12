8da8ebf080544382ce4bb1caf173dae2e6358cdb
onlyokok <126917421+onlyokok@users.noreply.github.com>
2023-10-12 22:53:23 +0100

    a

--- src/games/deepwoken.lua
+++ src/games/deepwoken.lua
@@ -16,6 +16,8 @@ pcall(function()
     })
 
     getgenv().ScriptOptions = {
+        Fly = false,
+        FlySpeed = 50,
         WalkSpeed = false,
         Speed = 1,
         InfiniteJump = false,
@@ -32,22 +34,88 @@ pcall(function()
     local LocalPlayerGroupBox = Tabs.Main:AddLeftGroupbox('Local Player')
 
     LocalPlayerGroupBox:AddToggle('MyToggle', {
+        Text = 'Fly',
+        Default = false,
+        Tooltip = 'Fly',
+    
+        Callback = function(Value)
+            getgenv().ScriptOptions.Fly = Value
+            while getgenv().ScriptOptions.Fly do task.wait()
+                pcall(function()
+                    local Velocity = Vector3.new(0, 5, 0)
+                    if not game:GetService("UserInputService"):GetFocusedTextBox() then
+                        if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.W) then
+                            Velocity += (workspace.CurrentCamera.CFrame.LookVector * getgenv().ScriptOptions.FlySpeed)
+                        end
+                
+                        if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.S) then
+                            Velocity += (workspace.CurrentCamera.CFrame.LookVector * -getgenv().ScriptOptions.FlySpeed)
+                        end
+                
+                        if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.A) then
+                            Velocity += (workspace.CurrentCamera.CFrame.RightVector * -getgenv().ScriptOptions.FlySpeed)
+                        end
+                
+                        if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.D) then
+                            Velocity += (workspace.CurrentCamera.CFrame.RightVector * getgenv().ScriptOptions.FlySpeed)
+                        end
+                    end
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Velocity
+                end)
+            end
+        end
+    }):AddKeyPicker('KeyPicker', {
+        Default = '',
+        SyncToggleState = true,
+    
+    
+
+        Mode = 'Toggle',
+    
+        Text = 'Fly',
+        NoUI = false,
+    
+        Callback = function(Value)
+            
+        end,
+    
+        ChangedCallback = function(New)
+            
+        end
+    })
+
+    LocalPlayerGroupBox:AddToggle('MyToggle', {
         Text = 'Walk Speed',
         Default = false,
         Tooltip = 'Walk Speed',
     
         Callback = function(Value)
             getgenv().ScriptOptions.WalkSpeed = Value
+            while getgenv().ScriptOptions.WalkSpeed do task.wait()
+                pcall(function()
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame += game.Players.LocalPlayer.Character.Humanoid.MoveDirection * getgenv().ScriptOptions.Speed
+                end)
+            end
+        end
+    }):AddKeyPicker('KeyPicker', {
+        Default = '',
+        SyncToggleState = true,
+    
+    
+
+        Mode = 'Toggle',
+    
+        Text = 'Walk Speed',
+        NoUI = false,
+    
+        Callback = function(Value)
+            
+        end,
+    
+        ChangedCallback = function(New)
+            
         end
     })
-
-    task.spawn(function()
-        while getgenv().ScriptOptions.WalkSpeed do
-            pcall(function()
-                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame += game.Players.LocalPlayer.Character.Humanoid.MoveDirection * getgenv().ScriptOptions.Speed
-            end)
-        end
-    end)
 
     LocalPlayerGroupBox:AddToggle('MyToggle', {
         Text = 'Infinite Jump',
@@ -56,19 +124,36 @@ pcall(function()
     
         Callback = function(Value)
             getgenv().ScriptOptions.InfiniteJump = Value
+            game:GetService("UserInputService").InputBegan:Connect(function(Input, Processed)
+                pcall(function()
+                    if Processed then return end
+                    if Input.KeyCode == Enum.KeyCode.Space then
+                        if getgenv().ScriptOptions.InfiniteJump then
+                            game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0, getgenv().ScriptOptions.Velocity, 0)
+                        end
+                    end
+                end)
+            end)
+        end
+    }):AddKeyPicker('KeyPicker', {
+        Default = '',
+        SyncToggleState = true,
+    
+    
+
+        Mode = 'Toggle',
+    
+        Text = 'Infinite Jump',
+        NoUI = false,
+    
+        Callback = function(Value)
+            
+        end,
+    
+        ChangedCallback = function(New)
+            
         end
     })
-
-    game:GetService("UserInputService").InputBegan:Connect(function(Input, Processed)
-		pcall(function()
-			if Processed then return end
-			if Input.KeyCode == Enum.KeyCode.Space then
-				if getgenv().ScriptOptions.InfiniteJump then
-                    game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0, getgenv().ScriptOptions.Velocity, 0)
-                end
-			end
-		end)
-	end)
 
     LocalPlayerGroupBox:AddToggle('MyToggle', {
         Text = 'No Fall Damage',
@@ -81,10 +166,23 @@ pcall(function()
     })
 
     LocalPlayerGroupBox:AddSlider('MySlider', {
+        Text = 'Fly',
+        Default = 50,
+        Min = 50,
+        Max = 250,
+        Rounding = 1,
+        Compact = false,
+    
+        Callback = function(Value)
+            getgenv().ScriptOptions.FlySpeed = Value
+        end
+    })
+
+    LocalPlayerGroupBox:AddSlider('MySlider', {
         Text = 'Walk Speed',
-        Default = 5,
+        Default = 10,
         Min = 1,
-        Max = 6,
+        Max = 20,
         Rounding = 1,
         Compact = false,
     
